c7625d031ab61bee93372ccd49382e18e751119c
Lucas Rodrigues <lukinhasr1423@gmail.com>
2024-05-05 17:11:36 -0300

    refactored revenue controller and service

--- src/main/java/com/lucasdev/financerto/controller/RevenueController.java
+++ src/main/java/com/lucasdev/financerto/controller/RevenueController.java
@@ -18,37 +18,40 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class RevenueController {
 
     @Autowired
-    private RevenueRepository revenueRepository;
-
-    @Autowired
     private RevenueService revenueService;
 
     @PostMapping
     @Transactional
     public ResponseEntity<RevenueResponseDTO> registerRevenue(@RequestBody @Valid RevenueDTO data, UriComponentsBuilder uriComponentsBuilder, Authentication authentication) {
-        return this.revenueService.registerRevenue(data, uriComponentsBuilder, authentication);
+        RevenueResponseDTO dto = this.revenueService.registerRevenue(data, authentication);
+        var uri = uriComponentsBuilder.path("/revenue/{id}").buildAndExpand(dto.id()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<RevenueResponseDTO> findRevenueById(@PathVariable String id, Authentication authentication) {
-        return this.revenueService.findRevenueById(id, authentication);
+        RevenueResponseDTO dto = this.revenueService.findRevenueById(id, authentication);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
     public ResponseEntity<Page<RevenueResponseDTO>> listRevenues(@PageableDefault(size = 10, sort = {"date"}, direction = Sort.Direction.DESC) Pageable pageable, Authentication authentication) {
-        return this.revenueService.listRevenues(pageable, authentication);
+        Page<RevenueResponseDTO> page = this.revenueService.listRevenues(pageable, authentication);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
     public ResponseEntity<RevenueResponseDTO> updateRevenue(@RequestBody @Valid RevenueUpdateDTO data, Authentication authentication) {
-        return this.revenueService.updateRevenue(data, authentication);
+        RevenueResponseDTO revenueUpdated = this.revenueService.updateRevenue(data, authentication);
+        return ResponseEntity.ok(revenueUpdated);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity deleteRevenue(@PathVariable String id, Authentication authentication) {
-        return this.revenueService.deleteRevenue(id, authentication);
+        this.revenueService.deleteRevenue(id, authentication);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/lucasdev/financerto/domain/revenue/RevenueService.java
+++ src/main/java/com/lucasdev/financerto/domain/revenue/RevenueService.java
@@ -2,13 +2,12 @@ package com.lucasdev.financerto.domain.revenue;
 
 import com.lucasdev.financerto.domain.user.User;
 import com.lucasdev.financerto.utils.RecoverAuthenticatedUser;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class RevenueService {
@@ -19,47 +18,44 @@ public class RevenueService {
     @Autowired
     private RevenueRepository revenueRepository;
 
-    public ResponseEntity<RevenueResponseDTO> registerRevenue(RevenueDTO data, UriComponentsBuilder uriComponentsBuilder, Authentication authentication) {
+    public RevenueResponseDTO registerRevenue(RevenueDTO data, Authentication authentication) {
         User currentUser = this.recoverAuthenticatedUser.getCurrentUser(authentication);
         Revenue revenue = new Revenue(currentUser, data);
         revenueRepository.save(revenue);
-        var uri = uriComponentsBuilder.path("/revenue/{id}").buildAndExpand(revenue.getId()).toUri();
-        return ResponseEntity.created(uri).body(new RevenueResponseDTO(revenue));
+        return new RevenueResponseDTO(revenue);
     }
 
-    public ResponseEntity<RevenueResponseDTO> findRevenueById(String id, Authentication authentication) {
+    public RevenueResponseDTO findRevenueById(String id, Authentication authentication) {
         User currentUser = this.recoverAuthenticatedUser.getCurrentUser(authentication);
         var revenue = revenueRepository.getReferenceById(id);
         if (!revenue.getUser().equals(currentUser)) {
-            return ResponseEntity.notFound().build();
+            throw new EntityNotFoundException();
         }
-        return ResponseEntity.ok(new RevenueResponseDTO(revenue));
+        return new RevenueResponseDTO(revenue);
     }
 
-    public ResponseEntity<Page<RevenueResponseDTO>> listRevenues(Pageable pageable, Authentication authentication) {
+    public Page<RevenueResponseDTO> listRevenues(Pageable pageable, Authentication authentication) {
         User currentUser = this.recoverAuthenticatedUser.getCurrentUser(authentication);
-        var page = revenueRepository.findAllByUserId(currentUser.getId(), pageable).map(RevenueResponseDTO::new);
-        return ResponseEntity.ok(page);
+        return revenueRepository.findAllByUserId(currentUser.getId(), pageable).map(RevenueResponseDTO::new);
     }
 
-    public ResponseEntity<RevenueResponseDTO> updateRevenue(RevenueUpdateDTO data, Authentication authentication) {
+    public RevenueResponseDTO updateRevenue(RevenueUpdateDTO data, Authentication authentication) {
         User currentUser = this.recoverAuthenticatedUser.getCurrentUser(authentication);
         var revenue = revenueRepository.getReferenceById(data.id());
         if (!revenue.getUser().equals(currentUser)) {
-            return ResponseEntity.notFound().build();
+            throw new EntityNotFoundException();
         }
         revenue.update(data);
-        return ResponseEntity.ok(new RevenueResponseDTO(revenue));
+        return new RevenueResponseDTO(revenue);
     }
 
-    public ResponseEntity deleteRevenue(String id, Authentication authentication) {
+    public void deleteRevenue(String id, Authentication authentication) {
         User currentUser = this.recoverAuthenticatedUser.getCurrentUser(authentication);
         Revenue revenue = revenueRepository.findById(id).orElse(null);
         if (revenue == null || !revenue.getUser().equals(currentUser)) {
-            return ResponseEntity.badRequest().build();
+            throw new EntityNotFoundException();
         }
         revenueRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 
 }
